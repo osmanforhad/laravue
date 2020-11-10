e4d4f538543823bf5fe7d7563ae4fcd37a2a6ef8
osman forhad <osmanforhad@pop-os.localdomain>
2020-11-10 15:53:57 +0600

    use vue router with laravel

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,7 +1,15 @@
 <template>
+<div>
 <v-container>
     <v-alert type="info">I'm Vuetify Component VAlert</v-alert>
+    <v-btn link to="/foo">Load Foo</v-btn>
+      <v-btn link to="/bar">Load Bar</v-btn>
+      <v-btn link to="/user/osman">Load User</v-btn>
 </v-container>
+<v-content>
+    <router-view></router-view>
+</v-content>
+</div>
 </template>
 
 <script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,10 +1,23 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+const foo = { template: "<v-alert type='error'>I'm Foo Component</v-alert>" }
+const bar = { template: "<v-alert type='error'>I'm Bar Component</v-alert>" }
+const user = { template: "<v-alert type='info'>I'm {{$route.params.name}}</v-alert>" }
 
 Vue.use(VueRouter)
 
-const routes = [
-
+const routes = [{
+        path: '/foo',
+        component: foo,
+    },
+    {
+        path: '/bar',
+        component: bar,
+    },
+    {
+        path: '/user/:name',
+        component: user,
+    }
 ]
 
 export default new VueRouter({ routes })
